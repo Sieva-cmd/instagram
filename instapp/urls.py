786955262fdb60045eb1e9e786440cf614635f13
Sieva-cmd <musyokasieva@gmail.com>
2022-03-30 13:28:48 +0300

    initial commit

---
+++ instapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+import instapp
+
+
+urlpatterns =[
+
+    path('',views.home,name ='home'),
+]
